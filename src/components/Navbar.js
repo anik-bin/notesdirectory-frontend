6ab26fed7fcb4e4128a96afe3266db6bddaf46d0
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-07-09 19:36:23 +0530

    Changed navbar style and added contact page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,34 @@
 import React from 'react'
-import {Link, useLocation, useNavigate} from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     let navigate = useNavigate();
-    const handleLogout = ()=>{
+    const handleLogout = () => {
         localStorage.removeItem('token');
         navigate("/login");
     }
 
     let location = useLocation();
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">NotesDirectory</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
-                        </li>
-
-                        <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} aria-current="page" to="/about">About</Link>
-                        </li>
+        <>
+            <nav className="nav-header">
+                <a className="nav-header-heading" href="/">Aniket Bindhani</a>
+                <div className="nav-header-links">
+                    <ul>
+                        <li><Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link></li>
+                        <li><Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link></li>
+                        <li><Link className={`nav-link ${location.pathname === "/contact" ? "active" : ""}`} to="/contact">Contact</Link></li>
                     </ul>
-                    {!localStorage.getItem('token')?<form className="d-flex" role="search">
-                        <Link className="btn btn-primary mx-2" to="/login" role="button">Login</Link>
-                        <Link className="btn btn-primary mx-2" to="/signup" role="button">Signup</Link>
-                    </form>: <button onClick={handleLogout} className="btn btn-primary">Logout</button>}
                 </div>
+            <div>
+                {!localStorage.getItem('token') ? <form>
+                    <button type="button" className='btn-1'><Link to="/login">Login</Link></button>
+                    <button type="button" className='btn-2'><Link to="/signup">Signup</Link></button>
+                </form> : <button type="button" className='btn-2' onClick={handleLogout}>Logout</button>}
             </div>
-        </nav>
+            </nav>
+        </>
+
     )
 }
 
