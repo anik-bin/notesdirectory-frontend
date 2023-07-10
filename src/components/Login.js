76c53961f2ae0a9e5f097d603a84a61f7b41b246
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-07-10 16:25:09 +0530

    changed style for login and signup component

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,21 +37,21 @@ const Login = (props) => {
         setCredentials({ ...credentials, [e.target.name]: e.target.value })
     }
     return (
-        <div className='formlogin'>
-            <h2 className='login-header'>Login Form</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" name="email" value={credentials.email} onChange={onChange} aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        <>
+            <h2>Login</h2>
+            <form action="" method='post' onSubmit={handleSubmit}>
+                <div class="note-form">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" value={credentials.email} onChange={onChange} required />
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange} />
+                <div class="note-form">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" value={credentials.password} onChange={onChange} required />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit">Login</button>
             </form>
-        </div>
+
+        </>
     )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -37,32 +37,27 @@ const Signup = (props) => {
 
     return (
         <>
-            <div className="container mt-3">
-                <h2>Signup Form</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" name="name" onChange={onChange} aria-describedby="emailHelp" />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" name="email" onChange={onChange} aria-describedby="emailHelp" />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" name="password" onChange={onChange} minLength={5} required />
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                        <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} minLength={5} required />
-                    </div>
-
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
+           
+            <h2>Signup</h2>
+            <form action="" method='post' onSubmit={handleSubmit}>
+                <div class="note-form">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" id="name" onChange={onChange} required />
+                </div>
+                <div class="note-form">
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" onChange={onChange} required />
+                </div>
+                <div class="note-form">
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" onChange={onChange} minLength={5} required />
+                </div>
+                <div class="note-form">
+                    <label htmlFor="cpassword">Confirm Password: </label>
+                    <input type="password" name="cpassword" id="cpassword" onChange={onChange} minLength={5} required />
+                </div>
+                <button type="submit">Signup</button>
+            </form>
         </>
     )
 }
