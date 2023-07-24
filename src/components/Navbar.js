833dea5578636051f7e23eafe740a477833ea7a4
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-07-24 21:56:32 +0530

    added comments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     let navigate = useNavigate();
@@ -8,23 +8,27 @@ const Navbar = () => {
         navigate("/login");
     }
 
-    let location = useLocation();
     return (
         <>
             <nav className="nav-header">
                 <Link className="nav-header-heading" to="/">Aniket Bindhani</Link>
                 <div className="nav-header-links">
                     <ul>
-                        <li><Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link></li>
-                        <li><Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="/about">About</Link></li>
-                        <li><Link className={`nav-link ${location.pathname === "/contact" ? "active" : ""}`} to="/contact">Contact</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </div>
             <div>
-                {!localStorage.getItem('token') ? <form>
+
+                {/* This is a conditional statement that checks if the token does not match then it shows login and signup button which means the user has not signed up or logged in yet and if the token matches then it shows the logout button */}
+                
+                {!localStorage.getItem('token') ? <form> 
                     <button type="button" className='btn-1'><Link to="/login">Login</Link></button>
                     <button type="button" className='btn-2'><Link to="/signup">Signup</Link></button>
                 </form> : <button type="button" className='btn-2' onClick={handleLogout}>Logout</button>}
+
+                
             </div>
             </nav>
         </>
